15102c23bc1e62850902b9053edde9ae1fb641be
antoinfive <antoinfive@gmail.com>
2016-09-07 10:15:03 -0400

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,28 @@
 describe('UserService', function () {
+	beforeEach(module('app'));
 
-});
+	var UserService, $httpBackend;
+
+	beforeEach(inject(function($injector) {
+			OurService = $injector.get('OurService');
+			$httpBackend = $injector.get('$httpBackend');
+
+			$httpBackend.when('GET', '/rest/user').respond({user: 'Scott Mescudi', email: 'kwzrd@wickedawesome.com'});
+	}));
+	
+	
+	it('should get the current users information', function(done) {
+				$httpBackend.expectGET('/rest/user/');
+
+				UserService
+					.getUser()
+					.then(function(res) {
+						var data = res.data;
+
+						if(data.user == "Scott Mesucdi") {
+							done();
+						};				
+		});	
+	});			
+});	
+
